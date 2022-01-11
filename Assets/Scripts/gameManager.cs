8f79dded89b232d7f3ee066231be3954412fc716
Bailym <bm39@hotmail.co.uk>
2022-01-11 21:06:38 +0000

    add score counter

--- Assets/Scripts/gameManager.cs
+++ Assets/Scripts/gameManager.cs
@@ -8,13 +8,17 @@ public class gameManager : MonoBehaviour
     public bool gameStarted;       //tap to start 
     public bool gameOver;           //player has died
     public TextMeshProUGUI scoreCounter;
+    public TextMeshProUGUI highScoreDisplay;
+    public TextMeshProUGUI gameOverText;
+    private int highScore;
     // Start is called before the first frame update
     void Start()
     {
         playerDied();
         scoreCounter.text = "Tap to Start!";
         gameOver = false;
-
+        highScoreDisplay.text = "High Score: " + PlayerPrefs.GetInt("High Score").ToString();
+        gameOverText.enabled = false;
     }
 
     // Update is called once per frame
@@ -29,6 +33,14 @@ public class gameManager : MonoBehaviour
         gameOver = true;
         gameStarted = false;
 
+        gameOverText.enabled = true;
+        gameOverText.text = "Game Over - Score: " + scoreCounter.text;
+
+
+        if (int.Parse(scoreCounter.text)> PlayerPrefs.GetInt("High Score"))
+        {
+            PlayerPrefs.SetInt("High Score", int.Parse(scoreCounter.text));
+        }
     }
 
     public void startGame()
@@ -44,4 +56,6 @@ public class gameManager : MonoBehaviour
         int newScore = int.Parse(scoreCounter.text) + 1;
         scoreCounter.text = newScore.ToString();
     }
+
+
 }
