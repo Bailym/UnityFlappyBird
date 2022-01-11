65023d17bef4455a468908daa768e6ce488fbf99
Bailym <bm39@hotmail.co.uk>
2022-01-11 18:10:40 +0000

    add pipe spawning and movement

---
+++ Assets/Scripts/spawnPipes.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnPipes : MonoBehaviour
+{
+
+    public List<GameObject> prefabPipes;
+    public Vector2 spawnPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnPipes", 0.5f, 1.5f);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        
+    }
+
+
+    void SpawnPipes()
+    {
+        int len = prefabPipes.Count;
+
+        int randNum = Random.Range(0, len);
+        Debug.Log(randNum);
+
+        Instantiate(prefabPipes[randNum], spawnPos, Quaternion.identity);
+    }
+}
